a296a404cc042f67220892e6d28b39bccac94497
OCCS Student <1ericainsworth@gmail.com>
2016-03-04 14:22:58 -0800

    Search feature working. #6

--- default.js
+++ default.js
@@ -31,42 +31,47 @@ var songs = [
 ]
 
 var search = document.getElementById("button");
- search.addEventListener("click", function (event) {
-   event.preventDefault();
+ search.addEventListener("click", function () {
    var keyword = document.getElementById('info');
    for (var i = 0; i < songs.length; i++) {
      if (songs[i].song === keyword.value) {
-       console.log(songs[i]);
+       matches(songs[i]);
      }
    }
  })
 
 var matches = function(hits) {
-var media = document.createElement("div");
-media.setAttribute("class", "media");
-var mediaLeft= document.createElement("div");
-mediaLeft.setAttribute("class", "media-left");
-var mediaLink = document.createElement("a");
-mediaLink.setAttribute("href", "#");
-var mediaImage = document.createElement("img");
-mediaImage.setAttribute("src", "class", "media-object");
-var mediaBody = document.createElement("div");
-mediaBody.setAttribute("class", "media-body");
-var mediaHeading = document.createElement("h3");
-mediaHeading.setAttribute("class", "media-heading");
-var userInfo = document.createElement("p");
-var uploadText = document.createElement("p");
+  var media = document.createElement("div");
+  media.setAttribute("class", "media");
+  var mediaLeft= document.createElement("div");
+  mediaLeft.setAttribute("class", "media-left");
+  var mediaLink = document.createElement("a");
+  mediaLink.setAttribute("href", "#");
+  //var mediaImage = document.createElement("img");
+  //mediaImage.setAttribute("src", "class", "media-object");
+  var mediaBody = document.createElement("div");
+  var newArtist = document.createTextNode(hits.artist);
+  mediaBody.setAttribute("class", "media-body");
+  //mediaBody.appendChild(mediaHeading);
 
-mediaHeading.appendChild(uploadText);
-mediaHeading.appendChild(userInfo);
-mediaBody.appendChild(mediaHeading);
-mediaLeft.appendChild(mediaBody);
-mediaLeft.appendChild(mediaImage);
-mediaLeft.appendChild(mediaLink);
-media.appendChild(mediaLeft);
+  var mediaHeading = document.createElement("h3");
+  var newText = document.createTextNode(hits.artist + " " + "|" + " " + hits.song);
+  mediaHeading.setAttribute("class", "media-heading");
+  mediaBody.appendChild(mediaHeading);
+
+  var userInfo = document.createElement("p");
+  var uploadText = document.createElement("p");
+
+  mediaHeading.appendChild(uploadText);
+  mediaHeading.appendChild(userInfo);
+  mediaHeading.appendChild(newText);
+
+  media.appendChild(mediaBody);
+//  mediaLeft.appendChild(mediaImage);
+  mediaLeft.appendChild(mediaLink);
+  media.appendChild(mediaLeft);
 
 
-
-
-
+  var results = document.getElementById('results');
+  results.appendChild(media);
 }
